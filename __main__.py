4f3bc0d225606ec559ed1ff7409c508e226e8f94
Jan Zmazek <janzmazek@eduroam-int-dhcp-97-50-119.ucl.ac.uk>
2017-02-21 15:30:09 +0000

    Saved variables to config file.

--- __main__.py
+++ __main__.py
@@ -1,21 +1,23 @@
 from matplotlib import pyplot as plt
 from matplotlib import animation
-
+import yaml
 import random
 
-bird_number = 50
-x_interval = [-450.0, 50.0]
-y_interval = [300.0, 600.0]
-x_velocity_interval = [0.0, 10.0]
-y_velocity_interval = [-20.0, 20.0]
-attraction_coefficient = 0.01
-reflection_limit = 100
-speeding_limit = 10000
-speeding_coeficient = 0.125
-x_axis = (-500, 1500)
-y_axis = (-500, 1500)
-frames = 200
-interval = 50
+with open("source/config.yaml", "r") as file:
+    config = yaml.load(file)
+bird_number = config['bird_number']
+x_interval = config['x_interval']
+y_interval = config['y_interval']
+x_velocity_interval = config['x_velocity_interval']
+y_velocity_interval = config['y_velocity_interval']
+attraction_coefficient = config['attraction_coefficient']
+reflection_limit = config['reflection_limit']
+speeding_limit = config['speeding_limit']
+speeding_coefficient = config['speeding_coefficient']
+x_axis = config['x_axis']
+y_axis = config['y_axis']
+frames = config['frames']
+interval = config['interval']
 
 # Starting  positions and velocities
 boids_x=[random.uniform(x_interval[0], x_interval[1]) for x in range(bird_number)]
@@ -43,8 +45,8 @@ def update_boids(boids):
     for i in range(bird_number):
         for j in range(bird_number):
             if (xs[j]-xs[i])**2 + (ys[j]-ys[i])**2 < speeding_limit:
-                xvs[i]=xvs[i]+(xvs[j]-xvs[i])*speeding_coeficient/bird_number
-                yvs[i]=yvs[i]+(yvs[j]-yvs[i])*speeding_coeficient/bird_number
+                xvs[i]=xvs[i]+(xvs[j]-xvs[i])*speeding_coefficient/bird_number
+                yvs[i]=yvs[i]+(yvs[j]-yvs[i])*speeding_coefficient/bird_number
     # Move according to velocities
     for i in range(bird_number):
         xs[i]=xs[i]+xvs[i]
@@ -52,7 +54,7 @@ def update_boids(boids):
 
 
 figure=plt.figure()
-axes=plt.axes(xlim=x_axis, ylim=y_axis)
+axes=plt.axes(xlim=(x_axis[0], x_axis[1]), ylim=(y_axis[0], y_axis[1]))
 scatter=axes.scatter(boids[0],boids[1])
 
 def animate(frame):
